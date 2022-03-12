86a8723c6ae472321c939b7b825bbf57f2c6dc29
K <90019300+everyonelovesyou@users.noreply.github.com>
2022-03-12 13:30:59 +0900

    update: ハンバーガーメニューの微調整
    幅の変更, アニメーションで動きを追加

--- docs/hamburger.js
+++ docs/hamburger.js
@@ -1,11 +1,12 @@
 $(function(){
     $(".btn-gnav").on("click", function(){
         // ハンバーガーメニューの位置を設定
-        var rightVal = 0;
         let vw = $(window).width();
+        var rightVal = 0;
+        
         if($(this).hasClass("open")) {
             // 位置を移動させメニューを開いた状態にする
-            rightVal =  50 - vw;
+            rightVal = vw * -0.75;
             // メニューを開いたら次回クリック時は閉じた状態になるよう設定
             $(this).removeClass("open");
         } else {
